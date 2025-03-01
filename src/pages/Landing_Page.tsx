1df84f67602a7931e40c6f09111002d7b2855b52
TheLuckyman30 <bnauta0@gmail.com>
2025-03-01 11:48:58 -0500

    Added intial page framework

---
+++ src/pages/Landing_Page.tsx
@@ -0,0 +1,14 @@
+import "./../css/Landing_Page.css"
+
+function LandingPage () {
+    return (
+        <div className="lp">
+            <div>
+                Landing Page
+            </div>
+        </div>
+        
+    )
+}
+
+export default LandingPage;
