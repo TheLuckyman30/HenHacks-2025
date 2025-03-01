d4980a61b5f142af7da5de0443d039d31f7ccfdf
TheLuckyman30 <bnauta0@gmail.com>
2025-03-01 12:55:34 -0500

    Added dropdown menu

--- src/pages/Landing_Page.tsx
+++ src/pages/Landing_Page.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "./../css/Landing_Page.css"
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 
 function LandingPage () {
+    const [button1Click, setbutton1Click] = useState<boolean>(false);
+    const [hcp, sethcp] = useState<string>("");
+
+    function handleChange (event: SelectChangeEvent) {
+        sethcp(event.target.value as string);
+    }
+
     return (
         <div className="lp">
             <div className="lp-title">
@@ -14,7 +23,20 @@ function LandingPage () {
                     Button 2
                 </div>
             </div>
-            
+            <div>
+                <FormControl style={{width: '5rem'}}>
+                    <InputLabel></InputLabel>
+                    <Select
+                    value={hcp}
+                    label=""
+                    onChange={handleChange}
+                    >
+                        <MenuItem value={"1"}>One</MenuItem>
+                        <MenuItem value={"2"}>Two</MenuItem>
+                        <MenuItem value={"3"}>Three</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
         </div>
         
     )
