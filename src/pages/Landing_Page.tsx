ef4ea3145ef4eb8da8f72fc83d35c972d93889f8
TheLuckyman30 <bnauta0@gmail.com>
2025-03-01 12:17:55 -0500

    Added inital buttons

--- src/pages/Landing_Page.tsx
+++ src/pages/Landing_Page.tsx
@@ -3,9 +3,18 @@ import "./../css/Landing_Page.css"
 function LandingPage () {
     return (
         <div className="lp">
-            <div>
-                Landing Page
+            <div className="lp-title">
+                Welcome to your Health Care Portal
             </div>
+            <div style={{display: "flex", flexDirection: 'row'}}>
+                <div className="lp-button">
+                    Button 1
+                </div>
+                <div className="lp-button">
+                    Button 2
+                </div>
+            </div>
+            
         </div>
         
     )
