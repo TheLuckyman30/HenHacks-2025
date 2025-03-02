40c93668fc449ccfa74580ec2052b489f7479342
Selin Bacaz <144861936+selinAbacaz@users.noreply.github.com>
2025-03-01 21:50:07 -0500

    Added background image

--- src/pages/Landing_Page.tsx
+++ src/pages/Landing_Page.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import './../css/Landing_Page.css';
 import FindForm from '../components/FindForm';
+import AddWave from '../components/wave';
+import zIndex from '@mui/material/styles/zIndex';
+import { Box } from '@mui/material';
+//import '../assets/backgroundImage.png';
 
 function LandingPage() {
   const INSURANCE_PROVIDERS = [
@@ -40,31 +44,40 @@ function LandingPage() {
   }
 
   return (
-    <div className="lp">
-      <div className="lp-title">Welcome to your Health Care Portal</div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <div onClick={formControl} className="lp-button">
-          Find doctors with insurance
+      <div className="lp background" >
+        
+        <section>
+        
+        <div className= "parent" style={{ display: 'flex', flexDirection: 'row' }}>
+        <div className= "child">
+
+example box 
+<Box></Box>
+</div>
+          <div onClick={formControl} className="lp-button">
+            Find doctors with insurance
+          </div>
         </div>
+        {showForm && (
+          <div style={{ position: 'fixed', zIndex: '1000' }}>
+            <FindForm
+              insuranceProviders={INSURANCE_PROVIDERS}
+              healthCareServices={HEALTHCARE_SERVICES}
+              distance={distance}
+              zipCode={zipCode}
+              insurance={insurance}
+              type={type}
+              setDistance={setDistance}
+              setZipCode={setzipCode}
+              setInsurance={setInurance}
+              setType={setType}
+              setShowForm={setShowForm}
+            ></FindForm>
+          </div>
+        )}
+        </section>
       </div>
-      {showForm && (
-        <div style={{ position: 'fixed', zIndex: '1000' }}>
-          <FindForm
-            insuranceProviders={INSURANCE_PROVIDERS}
-            healthCareServices={HEALTHCARE_SERVICES}
-            distance={distance}
-            zipCode={zipCode}
-            insurance={insurance}
-            type={type}
-            setDistance={setDistance}
-            setZipCode={setzipCode}
-            setInsurance={setInurance}
-            setType={setType}
-            setShowForm={setShowForm}
-          ></FindForm>
-        </div>
-      )}
-    </div>
+      
   );
 }
 
